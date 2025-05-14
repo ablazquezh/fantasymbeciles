03d81e4520d39bc67becfc3fa89c0295d4bcf131
ablazquezh <alb.blazquez.h@gmail.com>
2025-05-14 20:09:54 +0200

    Added field image to team view.

--- @components/leagueView/teamsView.tsx
+++ @components/leagueView/teamsView.tsx
@@ -22,89 +22,104 @@ const globalColnames = {
 const TeamsView: React.FC<TeamsViewProps> = ({participantData, game}) => {
 
     return (
-    <Paper
-        sx={{
-            gap: 2, // Espaciado entre imágenes
-            width: "fit-content", // Usa todo el ancho disponible
-            padding: 2,
-            mt: 10
-        }} 
-    >
-    
-        <Box >
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center',}}>
-                <img
-                src={`/static/teams/${game}/${String(participantData["team_name"]).replace("/", "_")}.png`} // Ruta basada en team_name
-                alt={participantData.team_name}
-                style={{
-                    width: "100%", // Ocupa todo el espacio disponible en su celda
-                    maxWidth: "45px", // Evita que sean demasiado grandes
-                    height: "auto",
-                    objectFit: "contain",
-                    borderRadius: "8px",
-                    padding: "5px",
-                    boxShadow: "2px 2px 10px rgba(0,0,0,0.2)",
-                    marginRight: "auto"
-                }}
-                />
-                <Typography gutterBottom variant="body1" component="div" sx={{margin: "auto"}}>
-                    N. jugadores: {participantData.players.length}
-                </Typography>
-            </div>
-
-            <TableContainer sx={{ minHeight: "200px", maxHeight: "550px", maxWidth: "310px", minWidth: "310px", overflowY: "auto" }} >
-
-                <Table stickyHeader>
-                <TableBody sx={{backgroundColor: '#fafafa'}}>
-
-                    {Object.keys(participantData.groupedPlayers).map((category) => (
-                    <React.Fragment key={category}>
-                        {/* Render category header */}
-                        <TableRow>
-                        <TableCell colSpan={3} style={{ fontWeight: "bold", backgroundColor: "#f1f1f1" }}>
-                            {category}
-                        </TableCell>
-                        </TableRow>
+    <Box sx={{display:"flex", padding: 2, mt: 10,
+                alignItems: 'center',   // Vertical alignment
+                justifyContent: 'center', // Horizontal alignment
+    }}>
+        <Paper
+            sx={{
+                gap: 2, // Espaciado entre imágenes
+                width: "fit-content", // Usa todo el ancho disponible
+                padding: 2,
+                ml: -40
+            }} 
+        >
         
-                        {/* Render rows for this category */}
-                        {participantData.groupedPlayers[category].map((row, index) => (
-                                            
-                            <TableRow sx={{ bgcolor: getRowColor(row.global_position), position:"relative"}} >
-                        
-                                { Object.keys(globalColnames).map((col) => (
+            <Box>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center',}}>
+                    <img
+                    src={`/static/teams/${game}/${String(participantData["team_name"]).replace("/", "_")}.png`} // Ruta basada en team_name
+                    alt={participantData.team_name}
+                    style={{
+                        width: "100%", // Ocupa todo el espacio disponible en su celda
+                        maxWidth: "45px", // Evita que sean demasiado grandes
+                        height: "auto",
+                        objectFit: "contain",
+                        borderRadius: "8px",
+                        padding: "5px",
+                        boxShadow: "2px 2px 10px rgba(0,0,0,0.2)",
+                        marginRight: "auto"
+                    }}
+                    />
+                    <Typography gutterBottom variant="body1" component="div" sx={{margin: "auto"}}>
+                        N. jugadores: {participantData.players.length}
+                    </Typography>
+                </div>
 
-                                    <TableCell key={col} >
-                                        {row[col as keyof typeof globalColnames] !== null &&
-                                        row[col as keyof typeof globalColnames] !== undefined ? (
-                                            Array.isArray(row[col as keyof typeof globalColnames]) ? (
-                                            (row[col as keyof typeof globalColnames] as string[]).map((item, index) => (
-                                                <Chip key={index} label={item} sx={{ margin: "2px" }} />
-                                            ))
-                                            ) : (
-                                            row[col as keyof typeof globalColnames]
-                                            )
-                                        ) : (
-                                            "-"
-                                        )}
-                                    </ TableCell>
+                <TableContainer sx={{ minHeight: "200px", maxHeight: "550px", maxWidth: "410px", minWidth: "410px", overflowY: "auto" }} >
 
-                                ))}
-                                        
+                    <Table stickyHeader>
+                    <TableBody sx={{backgroundColor: '#fafafa'}}>
+
+                        {Object.keys(participantData.groupedPlayers).map((category) => (
+                        <React.Fragment key={category}>
+                            {/* Render category header */}
+                            <TableRow>
+                            <TableCell colSpan={3} style={{ fontWeight: "bold", backgroundColor: "#f1f1f1" }}>
+                                {category}
+                            </TableCell>
                             </TableRow>
-                                    
-                        ))}
-                    </React.Fragment>
-                    ))}
-                    
-                </TableBody>
-                
-                </Table>
+            
+                            {/* Render rows for this category */}
+                            {participantData.groupedPlayers[category].map((row, index) => (
+                                                
+                                <TableRow sx={{ bgcolor: getRowColor(row.global_position), position:"relative"}} >
+                            
+                                    { Object.keys(globalColnames).map((col) => (
 
-            </TableContainer>
+                                        <TableCell key={col} >
+                                            {row[col as keyof typeof globalColnames] !== null &&
+                                            row[col as keyof typeof globalColnames] !== undefined ? (
+                                                Array.isArray(row[col as keyof typeof globalColnames]) ? (
+                                                (row[col as keyof typeof globalColnames] as string[]).map((item, index) => (
+                                                    <Chip key={index} label={item} sx={{ margin: "2px" }} />
+                                                ))
+                                                ) : (
+                                                row[col as keyof typeof globalColnames]
+                                                )
+                                            ) : (
+                                                "-"
+                                            )}
+                                        </ TableCell>
+
+                                    ))}
+                                            
+                                </TableRow>
+                                        
+                            ))}
+                        </React.Fragment>
+                        ))}
+                        
+                    </TableBody>
+                    
+                    </Table>
+
+                </TableContainer>
+                
+            </Box>
+                        
+        </Paper>
+
+        <Box sx={{display: 'flex',
+                alignItems: 'center',   // Vertical alignment
+                justifyContent: 'center', // Horizontal alignment
+                ml: 20}}
+        >
+            <img src="/static/field.png" alt="Logo" style={{height: "35vw"}}/>
             
         </Box>
-                    
-    </Paper>
+
+    </Box>
     )
 }
 
