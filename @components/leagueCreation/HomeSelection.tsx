26c9f38b7b6459f05b55a5ecd37742212872713e
ablazquezh <alb.blazquez.h@gmail.com>
2025-11-03 23:58:46 +0100

    cicd

--- @components/leagueCreation/HomeSelection.tsx
+++ @components/leagueCreation/HomeSelection.tsx
@@ -40,7 +40,7 @@ const HomeSelection: React.FC<StepOneProps> = ({ setStep }) => {
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  Salón de trofeos (checkk22k)
+                  Salón de trofeos (checkkeeqwe22k)
                 </Typography>
               </CardContent>
             </CardActionArea>
